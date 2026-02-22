ce6e15bca2bf8117bf7c4e3264e08f96722aa744
Atiquzzaman Snigdho <www.atik48@gmail.com>
2026-02-22 10:36:03 +0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://sassbackend.reachableads.com",
+        target: "https://sassbackend.reachableads.com",
         changeOrigin: true,
       },
     },
